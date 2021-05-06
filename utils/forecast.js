ddc7204c6ce5aefb11abc353df9a3e84b393197e
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-05-06 17:55:58 +0200

    Destructuring object

--- utils/forecast.js
+++ utils/forecast.js
@@ -5,22 +5,21 @@ const forecast = (latitude, longitude, callback) => {
     'http://api.weatherstack.com/current?access_key=91eccc99c52c925191c19d6744837705&query=' +
     latitude +
     ',' +
-    longitude +
-    '&units=f';
+    longitude;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
       callback(
         undefined,
-        response.body.current.weather_descriptions[0] +
+        body.current.weather_descriptions[0] +
           ', It is currently ' +
-          response.body.current.temperature +
+          body.current.temperature +
           " degress out. It's feel like " +
-          response.body.current.feelslike +
+          body.current.feelslike +
           ' degress out.'
       );
     }
--- utils/geocode.js
+++ utils/geocode.js
@@ -6,16 +6,16 @@ const geocode = (address, callback) => {
     encodeURIComponent(address) +
     '.json?access_token=pk.eyJ1IjoiZmF5c2FsMTMwIiwiYSI6ImNraTNzYjNrNTdlYWIyemw2MmJucnU5NXUifQ.Oqqwyn4wLKXHxL9XoaKGWg';
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to location services!', undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback('Unable to find location. Try another search.', undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
       });
     }
   });
