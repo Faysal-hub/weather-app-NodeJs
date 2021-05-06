ac94cf0cb1482d34b5e4fd24b92d8f69c0850d17
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-05-06 11:59:12 +0200

    customizing HTTP request

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const request = require('request');
 const url =
   'http://api.weatherstack.com/current?access_key=91eccc99c52c925191c19d6744837705&query=37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
-  // Parse the response body from JSON string into JavaScript object
-  const data = JSON.parse(response.body);
-  // Will print the current temperature to the console
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+      ' It is currently ' +
+      response.body.current.temperature +
+      " degress out. It's feel like " +
+      response.body.current.feelslike +
+      ' degress out.'
+  );
 });
